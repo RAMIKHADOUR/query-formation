385ccfa7eb015547fc29b0a2b61636bdfaa3dedf
RAMIKHADDOUR <87438424+RAMIKHADOUR@users.noreply.github.com>
2023-06-26 11:16:36 +0200

    jquery
    explication jquery

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+// let p = document.querySelectorAll('p')
+//p.style.display = "none"
+//$('p').hide();
+
+
+// var elements = document.querySelectorAll('p.test');
+// for(vari = 0 ; i < elements.length; i++){
+//     elements[i].getElementsByClassName.color = 'red';
+// }
+$('p.test').css('color','red');
+
+});
