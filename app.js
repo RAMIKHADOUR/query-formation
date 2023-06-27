4ab6c0801d9ff8ae651896e85f13c5b62e6b1a6d
RAMIKHADDOUR <87438424+RAMIKHADOUR@users.noreply.github.com>
2023-06-27 12:05:57 +0200

    creation ToDoList
    javascript,jquery

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-$(document).ready(function(){
-
-// let p = document.querySelectorAll('p')
-//p.style.display = "none"
-//$('p').hide();
-
-
-// var elements = document.querySelectorAll('p.test');
-// for(vari = 0 ; i < elements.length; i++){
-//     elements[i].getElementsByClassName.color = 'red';
-// }
-$('p.test').css('color','red');
-
+$(document).ready(function() {
+    $("#taskForm").submit(function(e) {
+        e.preventDefault();
+        let taskInput = $('input[type="text"].form-control');
+        let taskValue = taskInput.val() // taskInput.value
+ 
+        if (taskValue !== '') {
+            let taskText = $('<p>').text(taskValue);
+            let closeBtn = $('<button>').attr({
+                type: 'button',
+                class: 'btn-close',
+                'aria-label': 'Close'
+            });
+ 
+            let checkbox = $('<input>').attr({
+                type: 'checkbox',
+                class: 'form-check-input'
+            });
+ 
+            let taskItem = $('<div>').addClass('d-flex align-items-center');
+            taskItem.append(taskText).append(checkbox);
+            $('#toDo').children().append(taskItem);
+            taskText.before(closeBtn);
+        }
+    });
+ 
+    $(document).on('click', "button.btn-close", function(){
+        $(this).parent().remove();
+    })
+ 
+    $(document).on('click', "input[type='checkbox']", function() {
+        if ($(this).is(':checked')) {
+            let checkTask = $(this).parent();
+            $("#taskDone").children().append(checkTask);
+        }
+    });
 });
+
+```
